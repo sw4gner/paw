7d583bcc8aa3ab8dd457ed47e4c627ba519a592d
S W <fia4awagner@4261811f904c>
2019-08-27 14:52:04 +0000

    config

--- config.py
+++ config.py
@@ -2,30 +2,38 @@ import telepot
 import MySQLdb
 import urllib3
 
-
-
-telegram = {
-    'api_key': '716856099:AAFimr918ZMBaN3LG91WqSWe2cKYTqs7SRg',
-    'proxy_url': 'http://proxy.server:3128',
-    'hook_dom': 'https://swa9ner.pythonanywhere.com',
-    'hook_path': '/Telegram/2352',
-    'num_pools': 1,
-    'maxsize': 10,
-    'retries': False,
-    'timeout': 30,
-}
-
+sysuser = 547975832
+site = 'https://fia4awagner.pythonanywhere.com'
 proxy_url = "http://proxy.server:3128"
 telepot.api._pools = {
     'default': urllib3.ProxyManager(proxy_url=proxy_url, num_pools=3, maxsize=10, retries=False, timeout=30),
 }
 telepot.api._onetime_pool_spec = (urllib3.ProxyManager, dict(proxy_url=proxy_url, num_pools=1, maxsize=1, retries=False, timeout=30))
 
-bot = telepot.Bot(telegram['api_key'])
+def startBot(conf):
+    bot = telepot.Bot(conf['api_key'])
+    bot.setWebhook(site+conf['hook'], max_connections=1)
+    return bot
 
 def getCon():
     return MySQLdb.connect(
-        host='fia4awagner.mysql.pythonanywhere-services.com',
-        user='fia4awagner',
-        passwd='fia4awagnerfia4awagner',
-        database='fia4awagner$default')
+        host='',
+        user='',
+        passwd='',
+        database='')
+
+swagbot = {
+    'api_key': '',
+    'hook': '',
+}
+bot = startBot(swagbot)
+
+dntelegram = {
+    'api_key': '',
+    'hook': '',
+}
+botdnbot = None # startDnidB(dntelegram):
+
+
+def notifySysUser(msg):
+    bot.sendMessage(sysuser, msg, parse_mode='Markdown')
